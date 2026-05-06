8d0d7129ceb56893cef6f6fc523e5fbe7a732cbd
sorma <82502572+sorma@users.noreply.github.com>
2026-05-07 00:39:22 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,21 @@ import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
 
 if (process.env.NODE_ENV === 'development') {
   await initOpenNextCloudflareForDev();
-} 
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'cipcalor.it' }],
+        destination: 'https://www.cipcalor.it/:path*',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
