63051c5abb253953f52fa634b480b52bbc988b21
sorma <82502572+sorma@users.noreply.github.com>
2026-05-07 00:53:14 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,21 +7,6 @@ if (process.env.NODE_ENV === 'development') {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-
-      // Vecchie pagine
-      { source: '/lasnigo-e-mail', destination: '/contatti', permanent: true },
-      { source: '/lasnigo-e-mail/:path*', destination: '/contatti', permanent: true },
-      { source: '/news', destination: '/', permanent: true },
-      { source: '/news/:path*', destination: '/', permanent: true },
-      { source: '/i-nostri-mezzi', destination: '/mezzi', permanent: true },
-      { source: '/i-nostri-mezzi/:path*', destination: '/mezzi', permanent: true },
-      { source: '/chi-siamo', destination: '/', permanent: true },
-      { source: '/produzione', destination: '/prodotti', permanent: true },
-      { source: '/le-nostre-attivita', destination: '/servizi', permanent: true },
-    ]
-  },
 };
 
 export default nextConfig;
